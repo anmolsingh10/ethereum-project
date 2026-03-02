c42dbf17c0e9d809f8cf214095fc495925221fb3
iamanmolsingh0019@gmail.com <iamanmolsingh0019@gmail.com>
2026-03-02 10:40:59 +0000

    add secret scope for api key
    .

--- my_nft_project/main.py
+++ my_nft_project/main.py
@@ -1,24 +1,26 @@
 import asyncio
-from pyspark.sql import SparkSession
 from manager.nft_collection_manager import NFTCollectionManager
 from repository.collection_repository import CollectionRepository
 
-# Initialize Spark session
-spark = SparkSession.builder.getOrCreate()
+# Spark already exists in Serverless
+# DO NOT recreate unless needed
 
-ALCHEMY_KEY = "f9cmHEGLyyy_opmzDfQYA"  
-# Initialize manager
+ALCHEMY_KEY = dbutils.secrets.get(
+    scope="nft-scope",
+    key="ALCHEMY_KEY"
+)
+
 manager = NFTCollectionManager(spark, ALCHEMY_KEY)
 
-# Create collection once
 repo = CollectionRepository(spark)
+
 collections = {
     "collection_1": "0xBd3531dA5CF5857e7CfAA92426877b022e612cf8"
 }
+
 repo.create_collections(collections)
 
-# Fetch Base
 manager.fetch_base("collection_1")
 
-# Fetch Metadata
+# safer than await
 await manager.fetch_metadata("collection_1")
